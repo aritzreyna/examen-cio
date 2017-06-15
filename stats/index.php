1c4edf69e6afd42702a98543c8d22b47484d3113
aritzreyna <aritzreyna@gmail.com>
2017-06-15 13:32:29 +0200

    Add files via upload

---
+++ stats/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>LaLiga Stats</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link rel="icon" type="image/png" href="img/favicon_160.png">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans|Slabo+27px" rel="stylesheet">
+</head>
+
+<body>
+	<!--<div class="alice">a
+    </div>
+	<div class="blue1">b
+    </div>
+	<div class="blue2">c
+    </div>
+	<div class="blue3">d
+    </div>
+	<div class="green">e
+    </div>
+	<div class="venus">f
+    </div>
+    <div class="ath">g
+    </div>
+    <div class="ath2">h
+    </div>
+    <div class="ath3">i
+    </div>-->
+    <header>
+    	<nav class="menu">
+        	<ul class="menu-ul">
+            	<li class="menu-li"><a class="menu-a" href="index.php">Inicio</a></li>
+                <li class="menu-li"><a class="menu-a" href="">Equipos</a></li>
+                <li class="menu-li"><a class="menu-a" href="">Jugadores</a></li>
+                <li class="menu-li"><a class="menu-a" href="">Nosotros</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
