16839387cfe5b72d3b35d2c6da3e129b1654ae2b
aritzreyna <aritzreyna@gmail.com>
2017-06-21 14:01:01 +0200

    Add files via upload

--- stats/index.php
+++ stats/index.php
@@ -3,10 +3,17 @@
 <head>
 <meta charset="utf-8">
 <title>LaLiga Stats</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
 <link href="css/style.css" rel="stylesheet" type="text/css">
 <link rel="icon" type="image/png" href="img/favicon_160.png">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans|Slabo+27px" rel="stylesheet">
 
+
+  
 </head>
 
 <body>
@@ -30,7 +37,7 @@
     </div>-->
     <header>
     	<div class="logo-div">
-        	<h1 alt="LaLiga Stats">
+        	<h1 class="title-h1" alt="LaLiga Stats">
             	<a href="index.php"><img src="img/favicon_160.png" width="100%"><span>LaLiga Stats</span></a>
             </h1>
         </div>
@@ -55,31 +62,39 @@
                     </div>
                     <div class="form-player-detail">
                         <h3 class="in-form-title">Jugador en forma</h3>
-                        <h3 class="player-name">Iñaki Williams</h3>
-                        
+                        <h3 class="player-name"><a href="#">Iñaki Williams</a></h3>
+                        <h2 class="players-team"><img src="img/Club_Athletic_Bilbao_logo.svg" width="100%"> <span>Athletic Club</span></h2>
+                        <h2 class="players-info"><i class="material-icons icon-info">info_outline</i><span> 23 años. Delantero</span></h2>
                     </div>
                 </div>
                 <div class="stats-container">
                 	<div class="table-player-index">
                       <div class="stats-table-row">
-                        <div class="stats-table-cell match-title">
-                        	<img src="img/Club_Athletic_Bilbao_logo.svg" width="100%"> - <img src="img/Real_Sociedad_logo.svg" width="100%">
+                        <div class="stats-table-cell match-title"><span class="test" data-toggle="tooltip" title="Athletic Club - Real Sociedad">
+                        	<img src="img/Club_Athletic_Bilbao_logo.svg" width="100%"> - <img src="img/Real_Sociedad_logo.svg" width="100%"></span>
                         </div>
-                        <div class="stats-table-cell">T</div>
-                        <div class="stats-table-cell">TP</div>
-                        <div class="stats-table-cell">G</div>
-                        <div class="stats-table-cell">FC</div>
-                        <div class="stats-table-cell">FR</div>
-                        <div class="stats-table-cell">TA</div>
-                        <div class="stats-table-cell">TR</div>
-                        <div class="stats-table-cell">RI</div>
-                        <div class="stats-table-cell">RC</div>
-                        <div class="stats-table-cell">A</div>
-                        <div class="stats-table-cell">CI</div>
-                        <div class="stats-table-cell">CC</div>
-                        <div class="stats-table-cell">CD</div>
-                        <div class="stats-table-cell">CG</div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tiros">T</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tiros a puerta">TP</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Goles">G</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Asistencias">A</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Faltas cometidas">FC</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Faltas recibidas">FR</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Regates intentados">RI</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Regates completados">RC</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Centros intentados">CI</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Centros completados">CC</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tarjetas amarillas">TA</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tarjetas rojas">TR</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Cabezazos disputados">CD</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Cabezazos ganados">CG</span></div>
                       </div>
+                      
+                      <script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+
                       <div class="stats-table-row">
                         <div class="match-stats stats-value">3 - 1</div>
                         <div class="stats-table-cell stats-value">5</div>
@@ -102,7 +117,65 @@
                 </div>
 			</div>
 			<div class="form-team">
-				
+				<div class="main-info">
+                   <div class="form-team-photo-container">
+                        <img class="form-team-photo" src="img/Club_Athletic_Bilbao_logo.svg" alt="Athletic Club" width="100%">
+                   </div>
+                    <div class="form-player-detail">
+                        <h3 class="in-form-title">Equipo en forma</h3>
+                        <h3 class="player-name"><a href="#">Athletic Club</a></h3>
+                        <h2 class="players-team"><i class="material-icons icon-info">place</i><span>Bilbao</span></h2>
+                        <h2 class="players-info"><i class="material-icons icon-info">info_outline</i><span> Fundado en 1898. </span></h2>
+                    </div>
+                </div>
+                <div class="stats-container stats-container-right">
+                	<div class="table-player-index">
+                      <div class="stats-table-row">
+                        <div class="stats-table-cell match-title"><span class="test" data-toggle="tooltip" title="Athletic Club - Real Sociedad">
+                        	<img src="img/Club_Athletic_Bilbao_logo.svg" width="100%"> - <img src="img/Real_Sociedad_logo.svg" width="100%"></span>
+                        </div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tiros">T</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tiros a puerta">TP</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Goles">G</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Asistencias">A</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Faltas cometidas">FC</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Faltas recibidas">FR</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Regates intentados">RI</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Regates completados">RC</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Centros intentados">CI</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Centros completados">CC</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tarjetas amarillas">TA</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Tarjetas rojas">TR</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Cabezazos disputados">CD</span></div>
+                        <div class="stats-table-cell"><span class="test" data-toggle="tooltip" title="Cabezazos ganados">CG</span></div>
+                      </div>
+                      
+                      <script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+
+                      <div class="stats-table-row">
+                        <div class="match-stats stats-value">3 - 1</div>
+                        <div class="stats-table-cell stats-value">5</div>
+                        <div class="stats-table-cell stats-value">3</div>
+                        <div class="stats-table-cell stats-value">1</div>
+                        <div class="stats-table-cell stats-value">3</div>
+                        <div class="stats-table-cell stats-value">5</div>
+                        <div class="stats-table-cell stats-value">1</div>
+                        <div class="stats-table-cell stats-value">0</div>
+                        <div class="stats-table-cell stats-value">10</div>
+                        <div class="stats-table-cell stats-value">7</div>
+                        <div class="stats-table-cell stats-value">1</div>
+                        <div class="stats-table-cell stats-value">10</div>
+                        <div class="stats-table-cell stats-value">5</div>
+                        <div class="stats-table-cell stats-value">3</div>
+                        <div class="stats-table-cell stats-value">1</div>
+                      </div>
+                    </div>
+
+                </div>
 			</div>
     	</div>
 			
